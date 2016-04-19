e82f8cdd128ecf5ffa118b64b1c566ffe03482c6
The Buildbot <buildbot@angular2buch.de>
2016-04-19 17:29:56 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/205fc0e8608da5be9341052f4fd4dcc050b52274
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/124251661

---
+++ src/client/app/book-list/book-list.component.ts
@@ -0,0 +1,19 @@
+import {Component} from 'angular2/core';
+import {BookListItem} from '../book-list-item/book-list-item.component';
+import {Book} from '../../domain/book';
+import {BookStoreService} from '../services/books/book-store.service';
+
+@Component({
+  selector: 'book-list',
+  directives: [BookListItem],
+  moduleId: module.id,
+  templateUrl: 'book-list.component.html',
+  providers: [BookStoreService]
+})
+export class BookListComponent {
+  books: Book[];
+
+  constructor(private bs: BookStoreService) {
+    this.books = bs.getAll();
+  }
+}
