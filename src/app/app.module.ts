f5f97bffe6e9b885ac169f0e0127bedb926daeb0
The Buildbot <buildbot@angular2buch.de>
2016-10-08 21:15:51 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/7768f55393165c4b34c2303bc0213500d1c4fc14
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/166109872

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookListItemComponent } from './book-list-item/book-list-item.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+
+import { BookStoreService } from './shared/book-store.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent,
+    BookListItemComponent,
+    BookDetailsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    BookStoreService // NEW!
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
+
+@Component({
+  selector: 'bm-book-list',
+  templateUrl: 'book-list.component.html'
+})
+export class BookListComponent implements OnInit {
+  books: Book[];
+  @Output() showDetailsEvent = new EventEmitter<any>();
+
+  constructor(private bs: BookStoreService) { }
+
+  ngOnInit() {
+    this.books = this.bs.getAll();
+  }
+
+  showDetails(book: Book) {
+    this.showDetailsEvent.emit(book);
+  }
+}
